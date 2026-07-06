b8c834f281fc5eec4a968af75b817994ce6c63ab
fefarpari <fefarpari67@gmail.com>
2026-07-06 10:53:03 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,58 @@
+<?php
+$a=array(10,20,30);
+ 
+ echo $a[0]=50;
+ echo $a[1];
+ echo $a[2];
+ echo $a[1]=50;
+
+ echo "<br>";
+ 
+$b=array("atmiya","university","rajkot");
+
+  echo $b[0];
+  echo $b[1];
+  echo $b[2];
+  echo "<br>";
+  
+  //associative array
+$c=array('name'=>"computer",'city'=>"rajkot");
+
+foreach($c as $v)
+{
+	echo $v;
+}
+echo "<br>";
+echo $c['name'];
+echo $c['city'];
+echo "<br>";
+print_r($c);
+echo "<br>";
+var_dump($c);
+echo "<br>";
+
+ //multidimesional array
+ 
+$d=array(array(1,2,3),array("rajkot","atmiya","university"));
+for($i=0; $i<count($d); $i++)
+{
+	for($j=0; $j<=count($d); $j++)
+	{
+		echo $d[$i][$j];
+    }
+}
+echo "<br>";
+echo $d[0][0];
+echo $d[0][1];
+echo $d[0][2];
+echo $d[1][0];
+echo $d[1][1];
+echo $d[1][2];
+
+
+echo "<br>";
+
+
+
+ 
+?>
