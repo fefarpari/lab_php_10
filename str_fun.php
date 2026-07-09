d7e9d6d479f118657e34dc68a6c2f8ba99c3dc4e
fefarpari <fefarpari67@gmail.com>
2026-07-09 10:50:42 +0530

    Add files via upload

---
+++ str_fun.php
@@ -0,0 +1,28 @@
+<?php
+echo chr(97);
+echo "<br>";
+
+echo chr(65);
+echo "<br>";
+
+echo ord("b");
+echo "<br>";
+
+echo strtolower("ATMIYA");
+echo "<br>";
+
+echo strtoupper("atmiya");
+echo "<br>";
+
+echo md5("pari");
+echo "<br>";
+
+$a=explode(" ","this is test file");
+print_r($a);
+echo "<br>";
+
+
+echo "<br>";
+
+
+?>
