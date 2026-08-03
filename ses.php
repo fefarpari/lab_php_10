0aa4f4a07c020b77ee28ed00eada1ca80072769b
fefarpari <fefarpari67@gmail.com>
2026-08-03 11:38:14 +0530

    Add files via upload

---
+++ ses.php
@@ -0,0 +1,8 @@
+<form action="session.php" method="POST">
+	Username : <input type="text" name="username">
+	<br>
+	Password : <input type="text" name="pwd">
+	<br>
+	<input type="submit" name="submit">
+	
+</form>
